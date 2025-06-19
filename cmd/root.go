028f0ae206de177e238135e391eb4f7c7ebab537
Emmett Miller <emmett.miller@sky.uk>
2025-06-19 08:30:21 +0100

    [UPDATE] ci.yaml

--- cmd/root.go
+++ cmd/root.go
@@ -8,3 +8,12 @@ var RootCmd = &cobra.Command{
 	Use:   "appsync",
 	Short: "Synchronise 1AI application skeletons into tenant repos",
 }
+
+func init() {
+	cobra.OnInitialize()
+	RootCmd.PersistentFlags().StringP("token", "t", "", "GitHub access token")
+	err := RootCmd.MarkPersistentFlagRequired("token")
+	if err != nil {
+		return
+	}
+}
